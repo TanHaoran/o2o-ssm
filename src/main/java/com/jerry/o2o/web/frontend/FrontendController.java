4ddaf78246b7d026640f8869925600c6106ce2fa
Jerry <thrforever@126.com>
2018-07-03 11:58:54 +0800

    9-8 店铺详情页的开发下

--- src/main/java/com/jerry/o2o/web/frontend/FrontendController.java
+++ src/main/java/com/jerry/o2o/web/frontend/FrontendController.java
@@ -8,14 +8,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/frontend", method = { RequestMethod.GET })
 public class FrontendController {
 
+	/**
+	 * 首页路由
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/index")
 	private String index() {
 		return "frontend/index";
 	}
 
+	/**
+	 * 店铺列表路由
+	 * 
+	 * @return
+	 */
 	@RequestMapping(value = "/shopList")
 	private String shopList() {
 		return "frontend/shop-list";
+	}
+
+	/**
+	 * 店铺详情路由
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/shopDetail")
+	private String shopDetail() {
+		return "frontend/shop-detail";
 	}
 
 }
---
+++ src/main/webapp/resources/js/frontend/shop-detail.js
@@ -0,0 +1,143 @@
+$(function() {
+	var loading = false;
+	// 分页允许最大条数
+	var maxItems = 20;
+	// 默认一页返回的商品数
+	var pageSize = 10;
+
+	var listUrl = '/o2o/frontend/listProductsByShop';
+
+	var pageNum = 1;
+	var shopId = getQueryString('shopId');
+	var productCategoryId = '';
+	var productName = '';
+
+	var searchDivUrl = '/o2o/frontend/listShopDetailPageInfo?shopId=' + shopId;
+
+	getSearchDivData();
+	addItems(pageSize, pageNum);
+
+	function getSearchDivData() {
+		var url = searchDivUrl;
+		$
+				.getJSON(
+						url,
+						function(data) {
+							if (data.success) {
+								var shop = data.shop;
+								$('#shop-cover-pic').attr('src', shop.shopImg);
+								$('#shop-update-time').html(
+										new Date(shop.lastEditTime)
+												.Format("yyyy-MM-dd"));
+								$('#shop-name').html(shop.shopName);
+								$('#shop-desc').html(shop.shopDesc);
+								$('#shop-addr').html(shop.shopAddr);
+								$('#shop-phone').html(shop.phone);
+
+								// 获取店铺商品类别列表
+								var productCategoryList = data.productCategoryList;
+								var html = '';
+								productCategoryList
+										.map(function(item, index) {
+											html += '<a href="#" class="button" data-product-search-id='
+													+ item.productCategoryId
+													+ '>'
+													+ item.productCategoryName
+													+ '</a>';
+										});
+								$('#shopdetail-button-div').html(html);
+							}
+						});
+	}
+
+	function addItems(pageSize, pageIndex) {
+		// 拼接URL
+		var url = listUrl + '?' + 'pageIndex=' + pageIndex + '&pageSize='
+				+ pageSize + '&productCategoryId=' + productCategoryId
+				+ '&productName=' + productName + '&shopId=' + shopId;
+		loading = true;
+		$.getJSON(url, function(data) {
+			if (data.success) {
+				maxItems = data.count;
+				var html = '';
+				data.productList.map(function(item, index) {
+					html += '' + '<div class="card" data-product-id='
+							+ item.productId + '>'
+							+ '<div class="card-header">' + item.productName
+							+ '</div>' + '<div class="card-content">'
+							+ '<div class="list-block media-list">' + '<ul>'
+							+ '<li class="item-content">'
+							+ '<div class="item-media">' + '<img src="'
+							+ item.imgAddr + '" width="44">' + '</div>'
+							+ '<div class="item-inner">'
+							+ '<div class="item-subtitle">' + item.productDesc
+							+ '</div>' + '</div>' + '</li>' + '</ul>'
+							+ '</div>' + '</div>' + '<div class="card-footer">'
+							+ '<p class="color-gray">'
+							+ new Date(item.lastEditTime).Format("yyyy-MM-dd")
+							+ '更新</p>' + '<span>点击查看</span>' + '</div>'
+							+ '</div>';
+				});
+				$('.list-div').append(html);
+				var total = $('.list-div .card').length;
+				if (total >= maxItems) {
+					// 加载完毕，则注销无限加载事件，以防不必要的加载
+					$.detachInfiniteScroll($('.infinite-scroll'));
+					// 删除加载提示符
+					$('.infinite-scroll-preloader').remove();
+				}
+				pageNum += 1;
+				loading = false;
+				$.refreshScroller();
+			}
+		});
+	}
+
+	// 下滑屏幕自动分页搜索
+	$(document).on('infinite', '.infinite-scroll-bottom', function() {
+		if (loading) {
+			return;
+		}
+		addItems(pageSize, pageNum);
+	});
+
+	$('#shopdetail-button-div').on(
+			'click',
+			'.button',
+			function(e) {
+				productCategoryId = e.target.dataset.productSearchId;
+				if (productCategoryId) {
+					if ($(e.target).hasClass('button-fill')) {
+						$(e.target).removeClass('button-fill');
+						productCategoryId = '';
+					} else {
+						$(e.target).addClass('button-fill').siblings()
+								.removeClass('button-fill');
+					}
+					$('.list-div').empty();
+					pageNum = 1;
+					addItems(pageSize, pageNum);
+				}
+			});
+
+	$('.list-div').on(
+			'click',
+			'.card',
+			function(e) {
+				var productId = e.currentTarget.dataset.productId;
+				window.location.href = '/o2o/frontend/productDetail?productId='
+						+ productId;
+			});
+
+	$('#search').on('input', function(e) {
+		productName = e.target.value;
+		$('.list-div').empty();
+		pageNum = 1;
+		addItems(pageSize, pageNum);
+	});
+
+	$('#me').click(function() {
+		$.openPanel('#panel-left-demo');
+	});
+	$.init();
+});
