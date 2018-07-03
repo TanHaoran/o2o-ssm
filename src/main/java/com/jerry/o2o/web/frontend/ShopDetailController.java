dc0acdde7e98deda2f95312ccb8c83ceaa8ef899
Jerry <thrforever@126.com>
2018-07-03 11:40:59 +0800

    9-7 店铺详情页的开发上

---
+++ src/main/java/com/jerry/o2o/web/frontend/ShopDetailController.java
@@ -0,0 +1,109 @@
+package com.jerry.o2o.web.frontend;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jerry.o2o.dto.ProductExecution;
+import com.jerry.o2o.entity.Product;
+import com.jerry.o2o.entity.ProductCategory;
+import com.jerry.o2o.entity.Shop;
+import com.jerry.o2o.service.ProductCategoryService;
+import com.jerry.o2o.service.ProductService;
+import com.jerry.o2o.service.ShopService;
+import com.jerry.o2o.util.HttpServletRequestUtil;
+
+@Controller
+@RequestMapping("/frontend")
+public class ShopDetailController {
+
+	@Autowired
+	private ShopService shopService;
+
+	@Autowired
+	private ProductService productService;
+
+	@Autowired
+	private ProductCategoryService productCategoryService;
+
+	/**
+	 * 获取店铺信息以及店铺下的信息商品类别信息
+	 * 
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/listShopDetailPageInfo", method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> listShopDetailPageInfo(HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		long shopId = HttpServletRequestUtil.getLong(request, "shopId");
+		Shop shop = null;
+		List<ProductCategory> productCategoryList = null;
+		if (shopId != -1) {
+			shop = shopService.getByShopId(shopId);
+			productCategoryList = productCategoryService.getProductCategoryList(shopId);
+			modelMap.put("shop", shop);
+			modelMap.put("productCategoryList", productCategoryList);
+			modelMap.put("success", true);
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "empty shopId");
+		}
+		return modelMap;
+	}
+
+	@RequestMapping(value = "/listProductsByShop", method = RequestMethod.GET)
+	@ResponseBody
+	private Map<String, Object> listProductsByShop(HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		int pageIndex = HttpServletRequestUtil.getInt(request, "pageIndex");
+		int pageSize = HttpServletRequestUtil.getInt(request, "pageSize");
+		long shopId = HttpServletRequestUtil.getLong(request, "shopId");
+		if ((pageIndex > -1) && (pageSize > -1) && (shopId > -1)) {
+			long productCategoryId = HttpServletRequestUtil.getLong(request, "productCategoryId");
+			String productName = HttpServletRequestUtil.getString(request, "productName");
+			Product productCondition = compactProductCondition4Search(shopId, productCategoryId, productName);
+			ProductExecution pe = productService.getProductList(productCondition, pageIndex, pageSize);
+			modelMap.put("productList", pe.getProductList());
+			modelMap.put("count", pe.getCount());
+			modelMap.put("success", true);
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "empty pageSize or pageIndex or shopId");
+		}
+		return modelMap;
+	}
+
+	/**
+	 * 组合查询条件
+	 * 
+	 * @param shopId
+	 * @param productCategoryId
+	 * @param productName
+	 * @return
+	 */
+	private Product compactProductCondition4Search(long shopId, long productCategoryId, String productName) {
+		Product productCondition = new Product();
+		Shop shop = new Shop();
+		shop.setShopId(shopId);
+		productCondition.setShop(shop);
+		if (productCategoryId != -1L) {
+			ProductCategory productCategory = new ProductCategory();
+			productCategory.setProductCategoryId(productCategoryId);
+			productCondition.setProductCategory(productCategory);
+		}
+		if (productName != null && !productName.equalsIgnoreCase("null")) {
+			productCondition.setProductName(productName);
+		}
+		productCondition.setEnableStatus(1);
+		return productCondition;
+	}
+}
